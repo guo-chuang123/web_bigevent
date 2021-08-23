1c1626ccbf378aee830bc1928a57dcb422de8781
Oscar <2547239232@qq.com>
2021-08-23 15:51:33 +0800

    完成后台主页开发

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -2,4 +2,21 @@
 // 这个函数中可以拿到给Ajax提供的配置对象
 $.ajaxPrefilter(function(options){
     options.url="http://api-breakingnews-web.itheima.net"+options.url;
+
+    // 统一为有权限的接口,设置header请求头
+    if(options.url.indexOf("/my/")!==-1){
+        options.headers={
+            Authorization:localStorage.getItem("token")||''
+        }
+    }
+    
+    // 全局统一挂载complete函数
+    options.complete=function(res){
+         // res.responseJSON拿到服务器响应回来的数据
+         if(res.responseJSON.status ===1  && res.responseJSON.message==='身份认证失败！'){
+            // 强制清空token,强制跳转到登录界面
+            localStorage.removeItem("token");
+            location.href="/images/大事件/login.html";
+        }
+    }
 })
---
+++ assets/js/index.js
@@ -0,0 +1,58 @@
+$(function(){
+    getUserInfo();
+    var layer=layui.layer;
+    // 点击按钮退出登录
+    $("#btn_logout").on("click",function(){
+        layer.confirm('确定退出登录?', {icon: 3, title:'提示'}, function(index){
+            // 清空本地存储的token,并跳转到登录页
+            localStorage.removeItem('token');
+            location.href='/images/大事件/login.html';
+            
+            layer.close(index);
+          });
+    })
+})
+// 获取用户基本信息
+function getUserInfo(){
+    $.ajax({
+        method:"GET",
+        url:"/my/userinfo",
+        // headers就是请求头配置对象
+        // headers:{
+        //     Authorization:localStorage.getItem("token")||''
+        // },
+        success:function(res){
+            if(res.status !== 0){
+                return layui.layer.msg("获取用户信息失败！");
+            }
+            console.log(res);
+            // 渲染用户头像
+            renderAvatar(res.data);
+        },
+        // 无论成功失败,最终都会调用complete函数
+        // complete:function(res){
+        //     // res.responseJSON拿到服务器响应回来的数据
+        //     if(res.responseJSON.status ===1  && res.responseJSON.message==='身份认证失败！'){
+        //         // 强制清空token,强制跳转到登录界面
+        //         localStorage.removeItem("token");
+        //         location.href="/images/大事件/login.html";
+        //     }
+        // }
+    });
+}
+function renderAvatar(user){
+    // 获取用户昵称
+    var name=user.nickname||user.username;
+    $("#welcome").html('欢迎&nbsp;&nbsp;'+name);
+    // 按需渲染用户头像
+    if(user.user_pic !== null){
+        // 渲染图片头像
+        $(".layui-nav-img").attr('src',user.user_pic).show();
+        $(".text-avatar").hide();
+    }else{
+        // 渲染文本头像
+        $(".layui-nav-img").hide();
+        var first=name[0].toUpperCase();
+        $(".text-avatar").html(first).show();
+    }
+}
